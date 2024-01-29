c319c79e60e616101320e8393b83c83ba82c0753
Anenth <anenthvishnu@gmail.com>
2024-01-29 10:25:41 +0530

    Add google chat functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-/**
- * The entrypoint for the action.
- */
-import { run } from './main'
+import * as core from '@actions/core';
+import * as JobStatus from './status';
+import * as GoogleChat from './chat';
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+async function run() {
+  try {
+    const name = core.getInput('name', { required: true });
+    const url = core.getInput('url', { required: true });
+    const status = JobStatus.parse(core.getInput('status', { required: true }));
+
+    core.debug(`input params: name=${name}, status=${status}, url=${url}`);
+
+    await GoogleChat.notify(name, url, status);
+    console.info('Sent message.')
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else if (typeof error === 'string') {
+      core.setFailed(error);
+    } else {
+      core.setFailed('unexpected error');
+    }
+  }
+}
+
+run();
